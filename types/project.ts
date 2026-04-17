754e93e8479e4a96b4c4fe6b7451b340da62e747
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-04-17 22:57:43 +0100

    removed github workflow

--- types/project.ts
+++ types/project.ts
@@ -1,5 +1,7 @@
+// Project category type
 export type ProjectCategory = "web" | "security" | "automation"
 
+// Stores the front matter
 export type ProjectFrontmatter = {
   title: string
   slug: string
@@ -14,8 +16,10 @@ export type ProjectFrontmatter = {
   coverAlt?: string
 }
 
+// Exports the project summary
 export type ProjectSummary = ProjectFrontmatter
 
+// Exports the doc
 export type ProjectDoc = ProjectFrontmatter & {
   content: string
 }
