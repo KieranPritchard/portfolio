8c796eac188e4c03cba0aaa5abaf707da43db6bd
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-04-18 10:39:46 +0100

    created type for frontmatter

--- types/blog.ts
+++ types/blog.ts
@@ -1,2 +1,14 @@
 // Blog cateogory type
 export type BlogCategory = "developement" | "security" | "ai" | "review"
+
+// Type for the blog frontmatter
+export type BlogFrontmatter = {
+    title: string
+    slug: string
+    category: BlogCategory
+    description: string
+    tags: string[]
+    date: string
+    coverImage?: string
+    coverAlt: string
+}
