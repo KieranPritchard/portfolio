834bcae1f3728e16ecf47d8b7e9c18d906577a54
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-09-12 15:14:15 +0100

    added type fix

--- lib/certifications.ts
+++ lib/certifications.ts
@@ -5,15 +5,15 @@ import { Certification } from "@/types/certification"
 
 // Function to read in and parse the data
 export async function getAllCerts(): Promise<Certification[]> {
-    // Stores the content directory
-    const contentPath: string = path.join(process.cwd(), "public/main/certs.json")
+  // Stores the content directory
+  const contentPath: string = path.join(process.cwd(), "public/main/certs.json")
 
-    // Opens and reads the file into the program
-    const file: any = await fs.readFile(contentPath, "utf-8")
+  // Opens and reads the file into the program
+  const file = await fs.readFile(contentPath, "utf-8")
 
-    // Parses the data from the file
-    const data = JSON.parse(file)
+  // Parses the data from the file
+  const data = JSON.parse(file)
 
-    const list = Array.isArray(data) ? data : Object.values(data)
-    return list as Certification[]
+  const list = Array.isArray(data) ? data : Object.values(data)
+  return list as Certification[]
 }
